b2158e48d47863b8eeb1a307af195cb1c215b2bd
davidrz14 <davidrz1457@hotmail.com>
2022-08-24 18:59:25 -0500

    Update MovimientoDeDinero.java
    clase Movimiento Dinero

--- src/main/java/com/FM/Model/MovimientoDeDinero.java
+++ src/main/java/com/FM/Model/MovimientoDeDinero.java
@@ -13,5 +13,25 @@ public class MovimientoDeDinero {
     private Double monto;
     private String conceptoMovimientoDinero,rolEmpleado;
 
+public MovimientoDeDinero(Double monto, String conceptoMovimientoDinero, String rolEmpleado){
+    this.monto = monto;
+    this.conceptoMovimientoDinero = conceptoMovimientoDinero;
+    this.rolEmpleado = rolEmpleado;
+}
 
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public String getConceptoMovimientoDinero() {
+        return conceptoMovimientoDinero;
+    }
+
+    public void setConceptoMovimientoDinero(String conceptoMovimientoDinero) {
+        this.conceptoMovimientoDinero = conceptoMovimientoDinero;
+    }
 }
