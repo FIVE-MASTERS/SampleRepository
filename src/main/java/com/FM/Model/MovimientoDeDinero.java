d3d1523a6a2e4f1f899a89cc33c57535924aa7f3
Yeison-h <105256069+Yeison-h@users.noreply.github.com>
2022-08-31 18:34:51 -0500

    Update MovimientoDeDinero.java

--- src/main/java/com/FM/Model/MovimientoDeDinero.java
+++ src/main/java/com/FM/Model/MovimientoDeDinero.java
@@ -13,27 +13,4 @@ public class MovimientoDeDinero {
     private Double monto;
     private String conceptoMovimientoDinero;
     private Empleado empleado;
-
-
-public MovimientoDeDinero(Double monto, String conceptoMovimientoDinero, String rolEmpleado){
-    this.monto = monto;
-    this.conceptoMovimientoDinero = conceptoMovimientoDinero;
-    this.rolEmpleado = rolEmpleado;
-}
-
-    public Double getMonto() {
-        return monto;
     }
-
-    public void setMonto(Double monto) {
-        this.monto = monto;
-    }
-
-    public String getConceptoMovimientoDinero() {
-        return conceptoMovimientoDinero;
-    }
-
-    public void setConceptoMovimientoDinero(String conceptoMovimientoDinero) {
-        this.conceptoMovimientoDinero = conceptoMovimientoDinero;
-    }
-}
