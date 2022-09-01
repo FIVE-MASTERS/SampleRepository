443aa219e9576191579c94fa5d938fb17ed5fe47
yeisonhilarion <jdavidhg95@gmail.com>
2022-09-01 18:48:05 -0500

    acualizaciones

--- src/main/java/com/FM/Model/MovimientoDeDinero.java
+++ src/main/java/com/FM/Model/MovimientoDeDinero.java
@@ -11,27 +11,6 @@ import lombok.Setter;
 
 public class MovimientoDeDinero {
     private Double monto;
-    private String conceptoMovimientoDinero,rolEmpleado;
-
-public MovimientoDeDinero(Double monto, String conceptoMovimientoDinero, String rolEmpleado){
-    this.monto = monto;
-    this.conceptoMovimientoDinero = conceptoMovimientoDinero;
-    this.rolEmpleado = rolEmpleado;
-}
-
-    public Double getMonto() {
-        return monto;
-    }
-
-    public void setMonto(Double monto) {
-        this.monto = monto;
-    }
-
-    public String getConceptoMovimientoDinero() {
-        return conceptoMovimientoDinero;
-    }
-
-    public void setConceptoMovimientoDinero(String conceptoMovimientoDinero) {
-        this.conceptoMovimientoDinero = conceptoMovimientoDinero;
-    }
+    private String conceptoMovimientoDinero;
+    private Empleado empleado;
 }
