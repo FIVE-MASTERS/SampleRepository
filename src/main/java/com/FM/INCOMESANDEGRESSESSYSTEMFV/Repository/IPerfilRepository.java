3772e1cb2c976298c91a2a8f1cbc3d2503819f42
Yeison-h <105256069+Yeison-h@users.noreply.github.com>
2022-09-10 18:47:47 -0500

    Delete src/main/java/com/FM/INCOMESANDEGRESSESSYSTEMFV/Repository directory

--- src/main/java/com/FM/INCOMESANDEGRESSESSYSTEMFV/Repository/IPerfilRepository.java
+++
@@ -1,7 +0,0 @@
-package com.FM.INCOMESANDEGRESSESSYSTEMFV.repository;
-
-import com.FM.INCOMESANDEGRESSESSYSTEMFV.model.Perfil;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPerfilRepository extends JpaRepository<Perfil,Long> {
-}
