16e9487d1fedc802973481c4afa7c3ba8068355c
Yeison-h <105256069+Yeison-h@users.noreply.github.com>
2022-09-10 18:19:57 -0500

    Update Empleado.java

--- src/main/java/com/FM/INCOMESANDEGRESSESSYSTEMFV/Model/Empleado.java
+++ src/main/java/com/FM/INCOMESANDEGRESSESSYSTEMFV/Model/Empleado.java
@@ -1,74 +1,47 @@
 
-package com.FM.INCOMESANDEGRESSESSYSTEMFV.Model;
+package com.FM.INCOMESANDEGRESSESSYSTEMFV.model;
 
-<<<<<<< HEAD
-=======
-import lombok.AllArgsConstructor;
->>>>>>> ddd794d34acddf98bdb34091bb95197cd221f4ce
+import com.FM.INCOMESANDEGRESSESSYSTEMFV.enums.Enum_Rol;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-<<<<<<< HEAD
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Entity;
-import javax.persistence.GenerationType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Column;
-
-enum Rol_Enum {
-    ADMINISTRADOR, OPERATIVO
-}
-
-@Data
-=======
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
 
 
-enum Rol_Enum { ADMINISTRADOR,OPERATIVO
-}
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
 @Table(name = "Empleados")
->>>>>>> ddd794d34acddf98bdb34091bb95197cd221f4ce
 @Entity
-@Table(name = "Empleado")
 public class Empleado {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< HEAD
-    private Long idEmpleado;
-    
-    @Column
-    private String nombreEmpleado, correo;
-    
-    @Column
-    private Rol_Enum rol;
-
-    @ManyToOne
-    @JoinColumn(name = "id_empresa")
-    private Empresa empresa;
-=======
     private long idEmpleado;
 
-    @Column(name = "nombre",nullable = false)
+    @Column(name = "nombre")
     private String nombreEmpleado;
 
-    @Column(nullable = false)
+    @Column(name = "correo_empleado")
     private String correo;
 
-    @ManyToOne
-    @JoinColumn(name = "nombre_empresa",nullable = false )
-    private Empresa empresa;
-
-    @Column(nullable = false)
-    private Rol_Enum rol;
+    @ManyToOne (cascade = CascadeType.ALL)
+    @JoinColumn(name = "empresa_id")
+    private Empresa empresaEmpleado;
 
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol")
+    private Enum_Rol rol;
 
->>>>>>> ddd794d34acddf98bdb34091bb95197cd221f4ce
-}
+
+    @OneToOne//(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_perfil")
+    private Perfil perfil;
+
+    @Column(name= "fecha_creacion")
+    private LocalDate createAt;
+
+    @Column(name= "fecha_actualizacion")
+    private LocalDate updateAt;
+
+    @OneToMany(mappedBy = "empleado")
+    private List<MovimientoDinero> movimientosdeDineros;
