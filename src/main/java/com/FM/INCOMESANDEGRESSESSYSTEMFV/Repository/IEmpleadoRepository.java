aff25b901c3975c0afa56b77792aabf35a503e4f
Yeison-h <105256069+Yeison-h@users.noreply.github.com>
2022-09-10 18:22:55 -0500

    Update IEmpleadoRepository.java

--- src/main/java/com/FM/INCOMESANDEGRESSESSYSTEMFV/Repository/IEmpleadoRepository.java
+++ src/main/java/com/FM/INCOMESANDEGRESSESSYSTEMFV/Repository/IEmpleadoRepository.java
@@ -1,14 +1,11 @@
-package com.FM.INCOMESANDEGRESSESSYSTEMFV.Repository;
+package com.FM.INCOMESANDEGRESSESSYSTEMFV.repository;
 
 
-import com.FM.INCOMESANDEGRESSESSYSTEMFV.Model.Empleado;
+import com.FM.INCOMESANDEGRESSESSYSTEMFV.model.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IEmpleadoRepository extends JpaRepository<Empleado,Long> {
-
-
-
-
+    
 }
