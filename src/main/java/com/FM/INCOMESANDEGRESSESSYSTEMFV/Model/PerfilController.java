e61f3adade38af7a22cee41a3c31801e4c484dc1
Yeison-h <105256069+Yeison-h@users.noreply.github.com>
2022-09-10 18:34:08 -0500

    Add files via upload

---
+++ src/main/java/com/FM/INCOMESANDEGRESSESSYSTEMFV/Model/PerfilController.java
@@ -0,0 +1,43 @@
+package com.FM.INCOMESANDEGRESSESSYSTEMFV.controller;
+
+import com.FM.INCOMESANDEGRESSESSYSTEMFV.model.Perfil;
+import com.FM.INCOMESANDEGRESSESSYSTEMFV.service.IPerfilService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PerfilController {
+
+    @Autowired
+    private IPerfilService perfilService;
+
+    @GetMapping("/profile")
+    public List<Perfil> readAll() throws Exception {
+        return perfilService.readAll();
+    }
+
+    @PostMapping("/profile")
+    public Perfil create(@RequestBody Perfil perfil) throws Exception {
+        return perfilService.create(perfil);
+    }
+
+
+    @GetMapping("/profile/{id}")
+    public Perfil readById(@PathVariable("id") Long id) throws Exception {
+        return perfilService.readById(id);
+    }
+
+    @PutMapping("/profile")
+    public Perfil update(@RequestBody Perfil perfil) throws Exception {
+        return perfilService.update(perfil);
+    }
+
+    @DeleteMapping("/profile/{id}")
+    public void delete(@PathVariable("id") Long id) throws Exception {
+        perfilService.delete(id);
+
+    }
+
+}
