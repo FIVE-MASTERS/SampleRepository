928620ae9f7226fad319dbff9214f61c5e4c57f4
Yeison-h <105256069+Yeison-h@users.noreply.github.com>
2022-09-10 18:16:52 -0500

    Update MovimientoDeDinero.java

--- src/main/java/com/FM/INCOMESANDEGRESSESSYSTEMFV/Model/MovimientoDeDinero.java
+++ src/main/java/com/FM/INCOMESANDEGRESSESSYSTEMFV/Model/MovimientoDeDinero.java
@@ -1,56 +1,40 @@
-package com.FM.INCOMESANDEGRESSESSYSTEMFV.Model;
+package com.FM.INCOMESANDEGRESSESSYSTEMFV.model;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-<<<<<<< HEAD
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-
-=======
 import javax.persistence.*;
-@AllArgsConstructor
-@NoArgsConstructor
->>>>>>> ddd794d34acddf98bdb34091bb95197cd221f4ce
+import java.time.LocalDate;
+
 @Data
+
 @Entity
-<<<<<<< HEAD
-@Table(name = "movimientoDeDinero")
-=======
-@Table(name = "MovientosdeDinero")
->>>>>>> ddd794d34acddf98bdb34091bb95197cd221f4ce
-public class MovimientoDeDinero {
+@Table(name = "Movimientos_de_Dinero")
+public class MovimientoDinero {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< HEAD
-    private Long idMovimientoDeDinero;
-=======
     private long idMovimientoDeDinero;
 
     @Column (name = "Monto",nullable = false)
->>>>>>> ddd794d34acddf98bdb34091bb95197cd221f4ce
     private Double monto;
 
-    @Column(nullable = false)
+    @Column(name = "concepto_transaccion")
     private String conceptoMovimientoDinero;
-<<<<<<< HEAD
-    // private Empleado empleado;
-=======
 
-    @ManyToOne
-    @JoinColumn(name =  "rol_empleado",nullable = false)
+    @ManyToOne (cascade = CascadeType.ALL)
+    @JoinColumn(name =  "id_empleado")
     private Empleado empleado;
 
 
->>>>>>> ddd794d34acddf98bdb34091bb95197cd221f4ce
+    @Column(name= "fecha_creacion")
+    private LocalDate createAt;
+
+    @Column(name= "fecha_actualizacion")
+    private LocalDate updateAt;
+
 }
+
 
 
 
